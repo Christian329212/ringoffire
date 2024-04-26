91ed0309b24f7cacad1d44533cb58d4502d2108e
Ruben Christian Costea <156915142+Christian329212@users.noreply.github.com>
2024-04-26 16:03:25 +0200

    update

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game = new Game;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      const poppedCard = this.game.stack.pop();
+      if (poppedCard !== undefined) {
+        this.currentCard = poppedCard as string;
+        this.pickCardAnimation = true;
+        console.log('New card: ' + this.currentCard);
+        console.log('Game is', this.game);
+        setTimeout(() => {
+          this.game.playedCard.push(this.currentCard);
+          this.pickCardAnimation = false;
+        }, 1000);
+      }
+    }
+  }
+
 
 }
+
