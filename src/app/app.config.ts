12614be8c8c0da6910993ede45d0c8d83094d020
Ruben Christian Costea <156915142+Christian329212@users.noreply.github.com>
2024-05-06 14:40:50 +0200

    collection error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-4f1ca","appId":"1:656347873280:web:9458390dbdf7b86a3298e5","storageBucket":"ring-of-fire-4f1ca.appspot.com","apiKey":"AIzaSyBYWFXFo1N8j9Wz8MuSHYsS36a_OpcElvc","authDomain":"ring-of-fire-4f1ca.firebaseapp.com","messagingSenderId":"656347873280"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
@@ -11,27 +11,36 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { GameInfoComponent } from '../game-info/game-info.component';
 import { MatCardModule } from '@angular/material/card';
+import { Firestore, FirestoreModule, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, MatDialogModule, MatInputModule, FormsModule, GameInfoComponent, MatCardModule],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, MatDialogModule, MatInputModule, FormsModule, GameInfoComponent, MatCardModule, FirestoreModule],
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  firestore: Firestore = inject(Firestore);
   pickCardAnimation = false;
   currentCard: string = '';
   game: Game = new Game;
+  game$!: Observable<any[]>;
 
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.newGame();
+    const gameCollection = collection(this.firestore, 'games');
+    this.game$ = collectionData(gameCollection);
+    this.game$.subscribe(game => {
+      console.log('Game update', game);
+    });
   }
 
   newGame() {
     this.game = new Game();
+    this.firestore.collection('games').add({ 'Hallo': 'Welt' });
   }
 
   takeCard() {
